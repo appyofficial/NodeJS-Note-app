a18b2bdd4324776f6fb7241d05aede8013edc902
appyofficial <appy70890@gmail.com>
2020-02-05 00:25:35 +0100

    v 0.1

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
 const fs = require('fs');
 const mynote = require('./note');
+const yargs = require('yargs');
+
+yargs.version('1.0.1');
+
+//Add
+yargs.command({
+    command: "add",
+    describe: "Add a note.",
+    handler: function () {
+        console.log("Adding note.");
+    }
+});
+
+//Remove
+yargs.command({
+    command: "remove",
+    describe: "Removing a note.",
+    handler: function () {
+        console.log("Removing note.");
+    }
+});
+
+//List
+yargs.command({
+    command: "list",
+    describe: "Listing all notes.",
+    handler: function () {
+        console.log("Listing all the notes.");
+    }
+});
+
+//Read
+yargs.command({
+    command: "read",
+    describe: "Reading notes",
+    handler: function () {
+        console.log("Reading all the notes.");
+    }
+});
+
+console.log(yargs.argv);
+
+
+
+
+
 
 //fs.writeFileSync('notes.txt', 'The note which you want to write');
 //fs.appendFileSync('notes.txt', ', This is an extra text');
+//console.log(mynote());
+//const command = process.argv[2];
 
-console.log(mynote());
