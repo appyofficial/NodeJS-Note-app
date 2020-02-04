8fb15281f0b7c8f963f33bbd160fd0b6bebcf4f6
appyofficial <appy70890@gmail.com>
2020-02-04 22:57:30 +0100

    v 0.1

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'The note which you want to write');
+//fs.writeFileSync('notes.txt', 'The note which you want to write');
+
+fs.appendFileSync('notes.txt', ', This is an extra text');
